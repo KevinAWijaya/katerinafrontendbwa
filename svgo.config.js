44bb5e1378a917402a45aeef2b89bf232bcc88a6
KevinAWijaya <kevinaw46@gmail.com>
2025-09-02 12:55:20 +0700

    setup project

---
+++ svgo.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // default nya akan menambahkan prefix pada id di file svg, yang bikin tailwind susah baca svg
+  // dikasih prefix ids ini agar tidak ditambahkan prefix
+  prefixIds: false,
+};
